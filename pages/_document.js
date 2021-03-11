74477a7c3f1dfaa899e9709865dd7d19c476e72a
nathanluu <nate1299@me.com>
2021-03-11 12:21:11 -0500

    Updated favicon

--- pages/_document.js
+++ pages/_document.js
@@ -1,23 +1,29 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, {Html, Head, Main, NextScript} from 'next/document'
 
 class MyDocument extends Document {
     static async getInitialProps(ctx) {
         const initialProps = await Document.getInitialProps(ctx)
-        return { ...initialProps }
+        return {...initialProps}
     }
 
     render() {
         return (
             <Html>
                 <Head>
+                    {/*Favicons*/}
+                    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+                    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+                    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+                    <link rel="manifest" href="/site.webmanifest"/>
+
                     {/*inter web font*/}
-                    <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+                    <link rel="stylesheet" href="https://rsms.me/inter/inter.css"/>
                     {/*font awesome icons*/}
                     <script src="https://kit.fontawesome.com/aeb9fcb31a.js" crossOrigin="anonymous"></script>
                 </Head>
                 <body>
-                <Main />
-                <NextScript />
+                <Main/>
+                <NextScript/>
                 </body>
             </Html>
         )
