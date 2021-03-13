66ce060a1ca02eff28bd9b72cda528f163ac1b04
nathanluu <nate1299@me.com>
2021-03-13 10:59:59 -0500

    Added dark mode

--- components/BlogCard.js
+++ components/BlogCard.js
@@ -1,19 +1,19 @@
 import React from "react";
 
 export default function ({title, desc, img, link}){
-    return <div className="flex flex-col rounded-xl shadow-lg border border-gray-200 overflow-hidden hover:opacity-75 focus:opacity-50">
+    return <div className="flex flex-col rounded-xl shadow-lg border border-gray-200 dark:border-gray-800 overflow-hidden hover:opacity-75 focus:opacity-50">
         <div className="flex-shrink-0">
             <img className="h-48 w-full object-cover"
                  src={img}
                  alt=""/>
         </div>
-        <div className="flex-1 bg-white p-6 flex flex-col justify-between">
+        <div className="flex-1 bg-white dark:bg-gray-800 p-6 flex flex-col justify-between">
             <div className="flex-1">
                 <a href={link} className="block hover:opacity-75 focus:opacity-50">
-                    <h3 className="text-xl leading-7 font-semibold text-gray-900">
+                    <h3 className="text-xl leading-7 font-semibold text-gray-900 dark:text-white">
                         {title}
                     </h3>
-                    <p className="mt-2 text-base leading-6 text-gray-500">
+                    <p className="mt-2 text-base leading-6 text-gray-500 dark:text-gray-400">
                         {desc}
                     </p>
                 </a>
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import BlogCard from "../components/BlogCard";
 import Head from "next/head";
 import ZoomModal from "../components/ZoomModal";
@@ -105,16 +105,18 @@ const pageData = {
 }
 
 const SkillSection = ({title, skills}) => (<div className="mb-8">
-    <h2 className="text-sm uppercase text-gray-700 font-medium mb-2">{title}</h2>
+    <h2 className="text-sm uppercase text-gray-700 font-medium mb-2 dark:text-gray-400">{title}</h2>
     <ul className="flex justify-start items-center flex-wrap">
         {skills.sort().map((skill, index) => <li key={index}
-                                                 className="border shadow-sm md:text-lg text-gray-800 py-1 px-3 rounded-full flex-shrink-0 mr-2 mb-2">
+                                                 className="border dark:border-gray-700 shadow-sm md:text-lg text-gray-800 dark:text-gray-300 py-1 px-3 rounded-full flex-shrink-0 mr-2 mb-2">
             {skill}
         </li>)}
     </ul>
 </div>)
 
 export default function Home() {
+    const [darkMode, setDarkMode] = useState(false);
+
     const openZoom = () => {
         if (window.confirm('You are about to join my personal meeting room. Are you sure you want to continue?')) {
             window.location.href = "https://zoom.nthnluu.com/"
@@ -136,103 +138,136 @@ export default function Home() {
 
         return greeting;
     }
-    return <>
+
+    useEffect(() => {
+        updateTheme()
+    }, []);
+
+    const updateTheme = () => {
+        // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+        if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+            setDarkMode(true)
+        } else {
+            setDarkMode(false)
+        }
+    }
+
+    const toggleDarkMode = () => {
+        if (localStorage.theme === 'dark') {
+            localStorage.theme = 'light'
+        } else {
+            localStorage.theme = 'dark'
+        }
+
+        updateTheme();
+    }
+
+
+    return <div className={darkMode && "dark"}>
         <Head>
             <title>Nathan Benavides-Luu</title>
         </Head>
-        <div className="max-w-5xl mx-auto px-4 mb-24">
-            <div className="flex justify-end items-center py-6 space-x-2">
-                <a className="top-link" href={pageData.socials.github}>
-                    <i className="fab fa-github"/>
-                    <span className="sr-only">GitHub</span>
-                </a>
-                <a className="top-link" href={pageData.socials.linkedIn}>
-                    <i className="fab fa-linkedin"/>
-                    <span className="sr-only">Linkedin</span>
-                </a>
-                <a className="top-link" href={pageData.socials.instagram}>
-                    <i className="fab fa-instagram"/>
-                    <span className="sr-only">Instagram</span>
-                </a>
-                <button onClick={openZoom} className="top-link">
-                    <i className="fas fa-video"/>
-                    <span className="sr-only">Zoom</span>
-                </button>
-            </div>
-            <div className="pb-12 text-gray-800">
-                <h1>
-                    <span className="text-2xl">{timeGreeting()}! 👋 My name is</span>
-                    <div className="text-3xl sm:text-4xl md:text-5xl font-bold mt-2">
-                        <div>Nathan Benavides-Luu.</div>
-                        <div>I build things for the web.</div>
-                    </div>
-                </h1>
+        <div className="bg-white dark:bg-gray-900">
+            <div className="max-w-5xl mx-auto px-4 mb-24">
+                <div className="flex justify-start sm:justify-end items-center py-6 space-x-2">
+                    <button onClick={toggleDarkMode} className="top-link">
+                        <i className={`fas ${darkMode ? "fa-sun" : "fa-moon"}`}/>
+                        <span className="sr-only">Switch to light mode</span>
+                    </button>
+                    <a className="top-link" href={pageData.socials.github}>
+                        <i className="fab fa-github"/>
+                        <span className="sr-only">GitHub</span>
+                    </a>
+                    <a className="top-link" href={pageData.socials.linkedIn}>
+                        <i className="fab fa-linkedin"/>
+                        <span className="sr-only">Linkedin</span>
+                    </a>
+                    <a className="top-link" href={pageData.socials.instagram}>
+                        <i className="fab fa-instagram"/>
+                        <span className="sr-only">Instagram</span>
+                    </a>
+                    <button onClick={openZoom} className="top-link">
+                        <i className="fas fa-video"/>
+                        <span className="sr-only">Zoom</span>
+                    </button>
+                </div>
+                <div className="pb-12 text-gray-800 dark:text-gray-100">
+                    <h1>
+                        <span className="text-2xl">{timeGreeting()}! 👋 My name is</span>
+                        <div className="text-3xl sm:text-4xl md:text-5xl font-bold mt-2">
+                            <div>Nathan Benavides-Luu.</div>
+                            <div>I build things for the web.</div>
+                        </div>
+                    </h1>
 
-                <p className="max-w-4xl mt-5 text-lg md:text-xl text-gray-500">
-                    I’m a first-year computer science student at Brown University, an incoming software engineering
-                    (STEP) intern at Google, and an entrepreneurial full-stack developer with a passion for education
-                    technology. With my experience building user-friendly websites and iOS apps, I
-                    explore ways to make technology more accessible and useful in public education.
-                </p>
-            </div>
-            <div className="relative pt-16 pb-24">
-                <div className="relative mx-auto">
-                    <div className="text-left">
-                        <h2 className="text-3xl leading-9 tracking-tight font-bold text-gray-800 sm:text-4xl sm:leading-10">
-                            Projects
-                        </h2>
-                    </div>
-                    <div className="mt-12 grid gap-5 mx-auto md:grid-cols-2 lg:max-w-none">
-                        {pageData.projects.map((project, index) => <BlogCard key={index} title={project.title}
-                                                                             desc={project.description}
-                                                                             img={project.image}
-                                                                             link={project.href}/>)}
+                    <p className="max-w-4xl mt-5 text-lg md:text-xl text-gray-500 dark:text-gray-400">
+                        I’m a first-year computer science student at Brown University, an incoming software engineering
+                        (STEP) intern at Google, and an entrepreneurial full-stack developer with a passion for
+                        education
+                        technology. With my experience building user-friendly websites and iOS apps, I
+                        explore ways to make technology more accessible and useful in public education.
+                    </p>
+                </div>
+                <div className="relative pt-16 pb-24">
+                    <div className="relative mx-auto">
+                        <div className="text-left">
+                            <h2 className="text-3xl leading-9 tracking-tight font-bold text-gray-800 sm:text-4xl sm:leading-10 dark:text-gray-100">
+                                Projects
+                            </h2>
+                        </div>
+                        <div className="mt-12 grid gap-5 mx-auto md:grid-cols-2 lg:max-w-none">
+                            {pageData.projects.map((project, index) => <BlogCard key={index} title={project.title}
+                                                                                 desc={project.description}
+                                                                                 img={project.image}
+                                                                                 link={project.href}/>)}
+                        </div>
                     </div>
                 </div>
+
+                <div>
+                    <h2 className="text-3xl font-bold mb-6 dark:text-gray-100">Experience</h2>
+                    <ExperienceSection experiences={pageData.experience}/>
+                </div>
+
+
+                <div className="pt-24">
+                    <h2 className="text-3xl font-bold mb-4 dark:text-gray-100">Skills</h2>
+                    {pageData.skills.map((section, index) => <SkillSection key={index} title={section.title}
+                                                                           skills={section.items}/>)}
+                </div>
+
+
             </div>
 
-            <div>
-                <h2 className="text-3xl font-bold mb-6">Experience</h2>
-                <ExperienceSection experiences={pageData.experience}/>
-            </div>
+            <footer className="bg-white dark:bg-gray-900">
+                <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-center lg:px-8">
+                    <div className="flex justify-center items-center space-x-6 md:order-2">
+                        <a href={pageData.socials.instagram} className="text-gray-400 hover:text-gray-500">
+                            <span className="sr-only">Instagram</span>
+                            <svg className="h-8 w-8" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                                <path fillRule="evenodd"
+                                      d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
+                                      clipRule="evenodd"/>
+                            </svg>
+                        </a>
 
+                        <a href={pageData.socials.github} className="text-gray-400 hover:text-gray-500">
+                            <span className="sr-only">GitHub</span>
+                            <svg className="h-8 w-8" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                                <path fillRule="evenodd"
+                                      d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+                                      clipRule="evenodd"/>
+                            </svg>
+                        </a>
 
-            <div className="pt-24">
-                <h2 className="text-3xl font-bold mb-4">Skills</h2>
-                {pageData.skills.map((section, index) => <SkillSection key={index} title={section.title}
-                                                                       skills={section.items}/>)}
-            </div>
-
-
+                        <a href={pageData.socials.linkedIn} className="text-gray-400 text-3xl hover:text-gray-500">
+                            <span className="sr-only">LinkedIn</span>
+                            <i className="fab fa-linkedin"/>
+                        </a>
+                    </div>
+                </div>
+            </footer>
         </div>
 
-        <footer className="bg-white">
-            <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-center lg:px-8">
-                <div className="flex justify-center items-center space-x-6 md:order-2">
-                    <a href={pageData.socials.instagram} className="text-gray-400 hover:text-gray-500">
-                        <span className="sr-only">Instagram</span>
-                        <svg className="h-8 w-8" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-                            <path fillRule="evenodd"
-                                  d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
-                                  clipRule="evenodd"/>
-                        </svg>
-                    </a>
-
-                    <a href={pageData.socials.github} className="text-gray-400 hover:text-gray-500">
-                        <span className="sr-only">GitHub</span>
-                        <svg className="h-8 w-8" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-                            <path fillRule="evenodd"
-                                  d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-                                  clipRule="evenodd"/>
-                        </svg>
-                    </a>
-
-                    <a href={pageData.socials.linkedIn} className="text-gray-400 text-3xl hover:text-gray-500">
-                        <span className="sr-only">LinkedIn</span>
-                        <i className="fab fa-linkedin"/>
-                    </a>
-                </div>
-            </div>
-        </footer>
-    </>
+    </div>
 }
